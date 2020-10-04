26a04890e5c46afc3a01b07ed32c37e81ceca334
Bria White <bria.awhite@gmail.com>
2020-10-04 13:21:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+
+# class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  
+#   def change 
+#     create_table :costume_stores do |t|
+#       t.string :name
+#       t.string :location
+#       t.integer :costume_inventory
+#       t.integer :number_of_employees
+#       t.string :still_in_business
+#       t.integer :opening_time
+#       t.integer :closing_time
+#     end 
+  
+# end 
